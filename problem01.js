a414cee665c012ab1c0e54073f9bd778f782f7a3
unknown <protexbeer41@gmail.com>
2025-08-09 20:38:17 +0600

    two problem completed

--- problem01.js
+++ problem01.js
@@ -2,12 +2,34 @@
 
 // leaf Year
 
-function isLeapYear(year){ 
-    if((year % 400 === 0)  || ((year % 4 === 0) && (year % 100 !== 0))){
-console.log(`${year} is leap year!`);
-    }
-    else {console.log (`${year} is not a leap year!`);
-        }
-}
+// function isLeapYear(year){ 
+//     if((year % 400 === 0)  || ((year % 4 === 0) && (year % 100 !== 0))){
+// console.log(`${year} is leap year!`);
+//     }
+//     else {console.log (`${year} is not a leap year!`);
+//         }
+// }
 
-isLeapYear(2020);
+// isLeapYear(2020);
+
+
+
+// problem solution 02
+
+// vowel hunting
+
+
+// const vowels = ["a", "e","i","o", "u", "A", "E", "I", "O", "U"]
+
+// function countVowels(sentence){
+//     let count = 0;
+//     const letters = Array.from(sentence);
+//     letters.forEach(function(value, index, array){
+//         if(vowels.includes(value)){
+//             count++;
+//         }
+//     });
+//     return count;
+// }
+
+// console.log(countVowels("I love Bangladesh and we are going to Cox's Bazar."))
