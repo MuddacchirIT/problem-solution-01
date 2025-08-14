8db7c2eae6f7a4eb83a919ec719b9cf05f8c6ed0
unknown <protexbeer41@gmail.com>
2025-08-14 17:54:25 +0600

    Assignment 04 problem 04 completed

--- problem01.js
+++ problem01.js
@@ -657,40 +657,65 @@
 // Assignment 04 started here
 // Problem 03 here
 
-let team1 = {  name  : "Germany",  foul  : 10,  cardY  : 1,  cardR  : 1 };
-let team2 = {  name  : "France",  foul  : 10,  cardY  : 2,  cardR  : 1 };
+// let team1 = {  name  : "Germany",  foul  : 10,  cardY  : 1,  cardR  : 1 };
+// let team2 = {  name  : "France",  foul  : 10,  cardY  : 2,  cardR  : 1 };
 
- function bestTeam(player1, player2){
-    if(Array.isArray(player1)||Array.isArray(player2)|| typeof player1 === "string" || typeof player2 === "string"){
-        return "Invalid"
+//  function bestTeam(player1, player2){
+//     if(Array.isArray(player1)||Array.isArray(player2)|| typeof player1 === "string" || typeof player2 === "string"){
+//         return "Invalid"
+//     }
+//     let players1Sum = 0;
+//     let players2Sum = 0;
+
+//     for(let key in player1){
+//         if(typeof player1[key] === "number"){
+//             players1Sum += player1[key]
+//         }
+//     }
+
+//       for(let key in player2){
+//         if(typeof player2[key] === "number"){
+//             players2Sum += player2[key]
+//         }
+//     }
+
+//     if(players1Sum < players2Sum){
+//         return `${player1.name}`
+
+//     }
+//     else if(players1Sum > players2Sum){
+//         return `${player2.name}`
+//     }
+//     else{
+//          return "Tie"}
+//     }
+
+//  const result = bestTeam(team1, team2);
+//  console.log(result);
+ 
+
+
+// -----------------------------------------------------------
+// Assignment 04 started here
+// Problem 04 here
+ 
+let a = [2, null, 6];
+let b = [2, undefined, 6]; 
+
+ function isSame(arr1,arr2){ 
+    if(!Array.isArray(arr1) && !Array.isArray(arr2)){
+       return "Invalid";
     }
-    let players1Sum = 0;
-    let players2Sum = 0;
-
-    for(let key in player1){
-        if(typeof player1[key] === "number"){
-            players1Sum += player1[key]
-        }
-    }
-
-      for(let key in player2){
-        if(typeof player2[key] === "number"){
-            players2Sum += player2[key]
-        }
-    }
-
-    if(players1Sum < players2Sum){
-        return `${player1.name}`
-
-    }
-    else if(players1Sum > players2Sum){
-        return `${player2.name}`
+    else if(Array.isArray(arr1) && Array.isArray(arr2) && typeof arr1.includes === "number" && typeof arr2.includes === "number"){
+        return "true"
     }
     else{
-         return "Tie"}
+    return "false"; 
     }
-
- const result = bestTeam(team1, team2);
+}
+ 
+ const result = isSame(a, b);
  console.log(result);
  
+
 
