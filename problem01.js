bdcdf4f0a0ed11f826753917ba28512f9bfecd06
unknown <protexbeer41@gmail.com>
2025-08-14 22:09:17 +0600

    Assignment 04 problem 05 completed

--- problem01.js
+++ problem01.js
@@ -609,35 +609,30 @@
 
 
 
-
+               // 13 August 2025  Deadlin 15 August 2025
 // -----------------------------------------------------------
 // Assignment 04 started here
 // Problem 01 here
 
 //  function totalFine(fare){
 //      if(fare <= 0 || typeof fare != "number"){
-//         return "Invalid"
-//     }
-    
+//         return "Invalid"} 
+
 //     let surcharge = 0.2;
 //     let extraServiceCharge = 30;
 //     let fine = fare + (fare * surcharge) + extraServiceCharge;
 //     return fine;
 //     }
 
- 
- 
 // const result = totalFine(200);
 // console.log(result);
 
 
 
 // -----------------------------------------------------------
-// Assignment 04 started here
 // Problem 02 here
 
 //  function onlyCharacter(str){ 
-
 //      if(typeof str != "number"){
 //         let upperStr = str.toUpperCase();
 //         let noSpaceStr = upperStr.split(" ").join("");
@@ -654,14 +649,13 @@
 
 
 // -----------------------------------------------------------
-// Assignment 04 started here
 // Problem 03 here
 
 // let team1 = {  name  : "Germany",  foul  : 10,  cardY  : 1,  cardR  : 1 };
 // let team2 = {  name  : "France",  foul  : 10,  cardY  : 2,  cardR  : 1 };
 
 //  function bestTeam(player1, player2){
-//     if(Array.isArray(player1)||Array.isArray(player2)|| typeof player1 === "string" || typeof player2 === "string"){
+//     if(Array.isArray(player1)||Array.isArray(player2)|| typeof player1 ===   "string" || typeof player2 === "string"){
 //         return "Invalid"
 //     }
 //     let players1Sum = 0;
@@ -696,26 +690,70 @@
 
 
 // -----------------------------------------------------------
-// Assignment 04 started here
 // Problem 04 here
  
-let a = [2, null, 6];
-let b = [2, undefined, 6]; 
+// let a = [2, null, 6];
+// let b = [2, undefined, 6]; 
 
- function isSame(arr1,arr2){ 
-    if(!Array.isArray(arr1) && !Array.isArray(arr2)){
-       return "Invalid";
+//  function isSame(arr1,arr2){ 
+//     if(!Array.isArray(arr1) && !Array.isArray(arr2)){
+//        return "Invalid";
+//     }
+//     else if(Array.isArray(arr1) && Array.isArray(arr2) && typeof arr1.includes === "number" && typeof arr2.includes === "number"){
+//         return "true"
+//     }
+//     else{
+//     return "false"; 
+//     }
+// }
+ 
+//  const result = isSame(a, b);
+//  console.log(result);
+ 
+
+
+// -----------------------------------------------------------
+// Problem 05 here
+ 
+const input = [99];
+
+ function resultReport(marks)
+ { 
+        if(Array.isArray(marks) === false){
+            return "Invalid"
+        }
+        if(marks.length === 0){
+        return {finalScore: 0, pass: 0, fail: 0};
+
+        }
+    
+    let sum = 0;
+    for(const mark of marks){
+        sum = sum + mark;
+        }
+        const count = marks.length;
+        const avg = sum / count;
+        const score = avg.toFixed(2);
+        const finalScore = parseFloat(score);
+    
+    let pass = 0;
+    for (let i = 0; i < marks.length; i++) {
+    if (marks[i] >= 40) {
+      pass++;
+        }
     }
-    else if(Array.isArray(arr1) && Array.isArray(arr2) && typeof arr1.includes === "number" && typeof arr2.includes === "number"){
-        return "true"
+  
+    let fail = 0;
+    for (let i = 0; i < marks.length; i++) {
+    if (marks[i] <= 40) {
+    fail++;
+        }
     }
-    else{
-    return "false"; 
-    }
+    return {finalScore, pass, fail};
 }
- 
- const result = isSame(a, b);
- console.log(result);
- 
+      
+const result = resultReport(input);
+console.log(result);
 
 
+// ------------------------------END-----------------------------
